a25fd3652c61492160d7fdbc161897b374668a9c
Uciel Sola <solauciel@gmail.com>
2022-05-05 20:48:13 -0300

    removed env file

--- src/lib/services/graphql-client.js
+++ src/lib/services/graphql-client.js
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
-export const client = new GraphQLClient(import.meta.env.VITE_PUBLIC_ENDPOINT);
+export const client = new GraphQLClient("https://api-sa-east-1.graphcms.com/v2/cky0q3buw3y0b01xm3ggfb2te/master");
 
 export const storiesQuery = `query Stories {
 				stories {
