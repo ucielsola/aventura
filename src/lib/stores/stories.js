0fc34de62a3ef81f972899013d5e5c6059a558be
Uciel Sola <solauciel@gmail.com>
2022-05-05 20:27:00 -0300

    store simplified

--- src/lib/stores/stories.js
+++ src/lib/stores/stories.js
@@ -1,36 +1,30 @@
 import { writable } from 'svelte/store';
-import { client, storiesQuery } from '$lib/services/graphql-client.js';
+import { client, storiesQuery, storyQuery } from '$lib/services/graphql-client.js';
 
 export default function () {
-	const loading = writable(false);
-	const error = writable(false);
-	const stories = writable([]);
+	const stories = writable();
+	const story = writable();
+	const testVar = writable('');
 
 	async function getStories() {
-		loading.set(true);
-		error.set(false);
 		try {
 			const response = await client.request(storiesQuery);
-			stories.set(await response);
-			// console.log(response);
+			const array = await response.stories;
+			stories.set(array);
 		} catch (e) {
-			error.set(e);
+			console.log(e);
 		}
-		loading.set(false);
 	}
 
-	async function getStoryById() {
-		loading.set(true);
-		error.set(false);
+	async function getStoryById(storyId) {
 		try {
-			const response = await client.request(storiesQuery);
-			stories.set(await response);
-			// console.log(response);
+			const response = await client.request(storyQuery, { id: storyId });
+			const object = await response.story;
+			story.set(object);
 		} catch (e) {
-			error.set(e);
+			console.log(e);
 		}
-		loading.set(false);
 	}
 
-	return [stories, loading, error, getStories, getStoryById];
+	return [getStories, stories, getStoryById, story];
 }
